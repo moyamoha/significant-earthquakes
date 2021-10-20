3e8e7db5d2fdc0f5181e1e7183e52a4656b94d62
moyamoha <m.yahya.mohammadjavad@student.jyu.fi>
2021-10-20 07:02:21 +0300

    Poistin kommentit Maanjaristykset.js komponentista

--- src/Components/Maanjaristykset.js
+++ src/Components/Maanjaristykset.js
@@ -2,17 +2,6 @@ import React from 'react';
 import Maanjaristys from './Maanjaristys';
 
 const Maanjaristykset = ({ quakes, show }) => {
-    // const json = '{"result":true, "count":42}';
-    // const obj = JSON.parse(json);
-    // console.log(obj);
-
-    /* const sortable = [];
-    for (let i = 0; i < quakes.length; i += 1) {
-        const jsonObj = quakes[i];
-        sortable.push(jsonObj);
-    } */
-
-    // sortable.sort((a, b) => parseFloat(b.fields.i_d) - parseFloat(a.fields.i_d));
     quakes.sort((a, b) => a.localeCompare(b, 'en'));
 
     return (<div className="col-2">
