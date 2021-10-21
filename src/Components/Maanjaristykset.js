64c024b9f76a15c6deab8c72cd7dbc0d126229aa
moyamoha <moyamoha@jyu.fi>
2021-10-21 05:12:19 +0000

    Update Maanjaristykset.js

--- src/Components/Maanjaristykset.js
+++ src/Components/Maanjaristykset.js
@@ -4,7 +4,7 @@ import Maanjaristys from './Maanjaristys';
 const Maanjaristykset = ({ quakes, show }) => {
     quakes.sort((a, b) => a.fields.location_name.localeCompare(b.fields.location_name, 'en'));
 
-    return (<div className="col-2">
+    return (<div className="col-2 list-area">
             {quakes.map(jar =><Maanjaristys key={jar.recordid} jaristys={jar} show={show} />)}
         </div>
     );
