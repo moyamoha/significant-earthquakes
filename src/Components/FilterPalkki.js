88dc714f6e8eedaa84acea159bf7383e2a6d5365
moyamoha <m.yahya.mohammadjavad@student.jyu.fi>
2021-10-22 21:36:51 +0300

    Korvasin Maanjaristykset.js kokonaan komponentilla FilterPalkki.js

--- src/Components/FilterPalkki.js
+++ src/Components/FilterPalkki.js
@@ -4,13 +4,12 @@ import sampleData from '../data/earthquake_cut.json'
 const FilterPalkki = ({changeData}) => {
    
     const filterData = () => {
-        const eq_primary = document.getElementById
+        // Tässä filteroidaan dataa käyttäjän asettamien ehtojen perusteella
     }
-    return (<div className="col-2 listapalkki">
-                <h4>Filter map by:</h4>
-                <hr></hr>
-                <input type="range" min="1" max="10" onInput={{filterData}}></input>
-            </div>
+    return (
+        <div className="col-2 listapalkki">
+            {/* Täällä on tarkoituksena luoda erilaisia suodatusvaihtoehtoja */}
+        </div>
     );
 };
 
