f1080e8f069279806ba5507480a2aa140d19659b
moyamoha <m.yahya.mohammadjavad@student.jyu.fi>
2021-10-23 12:10:54 +0300

    12:10

--- src/Components/MyMap.js
+++ src/Components/MyMap.js
@@ -1,7 +1,7 @@
-import React  from "react";
-import { MapContainer, Marker, TileLayer , Popup,useMap} from "react-leaflet";
+import React,{onClick}  from "react";
+import { MapContainer, Marker, TileLayer , Popup,useMapEvents,MapConsumer} from "react-leaflet";
 import Data from "./../data/significant-earthquake-database.json";
-import geo from "./../data/geo.json"
+
 import 'leaflet/dist/leaflet.css';
 import L from 'leaflet';
 
@@ -19,54 +19,34 @@ L.Icon.Default.mergeOptions({
 
 function MyMap({data, setCurrentQuake}) {
     
-    
-    
-    function Paivita(piste){
-        console.log( piste.fields.country)
-    
-                          document.getElementById("sijainti").textContent = " " +  piste.fields.country;
-                          document.getElementById("voimakkuus").textContent = " " + piste.fields.eq_primary;
-                          document.getElementById("ajankohta").textContent = " " + piste.fields.year+"."+piste.fields.month+"."+piste.fields.day;
-    }
-      
-
-   
-       
-
-//const filtered = Data.filter(piste => piste.fields.intensity >10 &&piste.fields.coordinates !== undefined )
-
-
-
-function Pisteet() {
-    const map = useMap();
-    
-    
-    var myLayer = L.geoJSON(geo,{filter:suodatin}).addTo(map);
-     function suodatin(jaristys){
-         if(jaristys.properties.eq_primary >8) return true
-        
-     }
-     
-     
-
-
-
-    return null
-  }
-    
-   
-    return (
-        <MapContainer   className="map col-9 w-100" center={position} zoom={5} style={{height:"500px"}}>
-            <TileLayer
-                attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    function lisaa(){
+       return( Data.map(piste => (
+                
+            <Marker 
+             
+            /* key = {piste.fields.id} */
+             eventHandlers={{
+                 click: () => setCurrentQuake(piste)
+               }}
                
-            />
-            
-        <Pisteet/>
-
-
-        </MapContainer>
+             position = {piste.fields.coordinates}
+             >
+                <Popup>{piste.fields.country}</Popup>
+            </Marker>
+       
+         ))
+       )}
+    
+    return (
+        <div className="px-5 pt-4 pb-3">
+            <MapContainer   className="map col-9 w-100" center={position} zoom={5} style={{height:"500px"}}>
+                <TileLayer
+                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                {lisaa()}
+            </MapContainer>
+        </div>
     )
 
 }
